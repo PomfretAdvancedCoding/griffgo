b9afbc6ea2d1277b256ed5d0a36576290d47b5bb
hackerJake <jfarrell5678@gmail.com>
2017-03-28 14:58:11 -0400

    project delete

--- GriffGo/FirstViewController.swift
+++ GriffGo/FirstViewController.swift
@@ -17,11 +17,6 @@ class FirstViewController: UIViewController {
     
     var ref: FIRDatabaseReference!
     
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var greetingLabel: UILabel!
-    @IBOutlet weak var userPic: UIImageView!
-    @IBOutlet weak var loadingIcon: UIActivityIndicatorView!
-    
     @IBAction func logoutButton(_ sender: Any) {
         //Logout user
         UserData.sharedInstance.userID = 0
@@ -32,93 +27,9 @@ class FirstViewController: UIViewController {
         
     }
     
-    /*
-     *The profile picture of the student begins to load.
-     *The loading icon will pop up first while waiting for the photo to load
-     *If the photo is emty string, it will get data from alamofire to load the student's image. The picture will become circular and the loading icon will stop
-     */
-    
-    func picLoader() {
-        self.loadingIcon.startAnimating()
-        var photo = UserData.sharedInstance.userPhoto
-        if photo == "" {
-            photo = "large_user2723453_934847.png"
-        }
-        Alamofire.request("https://bbk12e1-cdn.myschoolcdn.com/ftpimages/14/user/\(photo)").responseImage { response in
-            if let image = response.result.value {
-                let circularImage = image.af_imageRoundedIntoCircle()
-                self.userPic.image = circularImage
-                self.loadingIcon.stopAnimating()
-            }
-        }
-        
-    }
-    /*
-     *This will determine the current time of the day and the date using NSDate
-     If the time is at the hour, it will display a certain message
-     */
     
     
-    func timeOfDay() -> String {
-        let date = NSDate()
-        let calendar = NSCalendar.current
-        let hour = calendar.component(.hour, from: date as Date)
-        
-        if hour > 20 && hour < 22 {
-            return "Hope you had a great day "
-        }
-        else if hour < 3 {
-            return "It's getting late "
-        }
-        else if hour < 12 {
-            return "Good morning "
-        }
-        else if hour < 17 {
-            return "Good afternoon "
-        }
-        else if hour >= 17 {
-            return "Good evening "
-        }
-        else {return "Hello "}
-    }
-    
-    /*
-     *This is the code to display the user name in the greetingLabel
-     *It can tell where the user is a student or a faculty
-     *It gather the data to determine the user's last name and prefix
-     *If the user's a student, it will return their nick name
-     *This and the timeOfDay label is the greeting.
-     
-     */
-    
-    func userGreeting () -> String {
-        //UserData.sharedInstance.userGreeting = UserData.sharedInstance.facultyData[index].photo!
-        let index = UserData.sharedInstance.userIndex
-        if UserData.sharedInstance.isFaculty == true {
-            let greeting = String(
-                UserData.sharedInstance.facultyData[index].prefix!
-                    + " " +
-                    UserData.sharedInstance.facultyData[index].lastName
-            )
-            return greeting!
-        }
-        else {
-            return UserData.sharedInstance.nickName
-        }
-    }
-    /*
-     *this determine the date of the day using NSDate
-     *This will display in the dateLABEL which is below the greeting label
-     
-     */
-    func date() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMMM d, yyyy"
-        let currentDate = NSDate()
-        return dateFormatter.string(from: currentDate as Date)
-    }
-    
-    /*
+        /*
      *This does not show up on the homescreen.
      *It is used to gather sport games high light from the the pomfretschool alamofire api
      */
@@ -171,16 +82,10 @@ class FirstViewController: UIViewController {
         //Load Firebase
         ref = FIRDatabase.database().reference()
         
-        //Function to Load User photo
-        picLoader()
         
         //SweetsTableViewController.loadData()
         
-        //Set Greeting label
-        greetingLabel.text = "\(timeOfDay())\(userGreeting())"
-        
-        //Set the date
-        dateLabel.text = "Today is \(date())"
+       
         
         //Get Athletic Data
         sportsGet()
